b3fec51949bdd9ecd5b3533028f35fbd4d3601ab
Macx <1271253083@qq.com>
2018-12-30 14:22:20 +0800

    insert

--- jdbcTest/src/com/jike/jdbc/JDBCTest.java
+++ jdbcTest/src/com/jike/jdbc/JDBCTest.java
@@ -3,10 +3,37 @@ package com.jike.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;;
+import java.sql.Statement;
+
+import org.apache.catalina.storeconfig.StandardHostSF;;
 
 public class JDBCTest {
-	public static void main(String[] args) {
+	public static Connection getConnection() {
+		Connection connection=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jsp_db?useUnicode=true&characterEncoding=utf8&useSSL=false","root","hg@564900");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	public static void insert() {
+		Connection connection=getConnection();
+		try {
+			String sql="INSERT INTO tbl_user(name,password,email)"+
+		"VALUES('Tom','123456','tom@gmail.com')";
+			Statement statement=connection.createStatement();
+			int count = statement.executeUpdate(sql);
+			System.out.println("向用户表中插入了"+count+" 条记录");
+			connection.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public static void selectAll() {
 		String sql ="SELECT * FROM tbl_user";
 		Connection connection=null;
 		Statement statement=null;
@@ -43,5 +70,9 @@ public class JDBCTest {
 				// TODO: handle exception
 			}
 		}
+	}
+	public static void main(String[] args) {
+		insert();
+		selectAll();
 	}
 }
