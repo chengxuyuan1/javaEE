52c249022740e356767677753bfceff4e636aebe
Macx <1271253083@qq.com>
2018-12-31 12:39:07 +0800

    loginServlet

---
+++ jdbcTest/WebContent/14/mylogin.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>登录页面</title>
+</head>
+<body>
+	<form action="<%=request.getContextPath() %>/loginServlet" method="get">
+		username:<input type="text"  name="uname"/> <br/>
+		password: <input type="password" name="upwd"/> <br/>
+		<input type="submit" value="login"/>
+		<input type="reset" value="Reset"/>
+	</form>
+</body>
+</html>
---
+++ jdbcTest/src/com/jike/servlet/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.jike.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5864346102781070171L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("=====进入doGet方法====");
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("=====进入doPost方法====");
+		String userName=req.getParameter("uname");
+		String password=req.getParameter("upwd");
+		System.out.println("用户名==》"+userName);
+		System.out.println("密码==》"+password);
+	}
+
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		String userName=req.getParameter("uname");
+//		String password=req.getParameter("upwd");
+//		System.out.println("用户名==》"+userName);
+//		System.out.println("密码==》"+password);
+//	}
+	
+}
